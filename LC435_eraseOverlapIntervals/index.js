c02432c7621a22fe9fb0537662ded6cb7fe658bf
Nishant <nishantindia18@gmail.com>
2020-04-23 09:50:26 +0530

    solution

---
+++ LC435_eraseOverlapIntervals/index.js
@@ -0,0 +1,32 @@
+const eraseOverlapIntervals = intervals => {
+    if(intervals.length === 0) return 0;
+    let count  = 0;
+
+    intervals.sort((a,b)=> {
+        return a[0]-b[0];
+    })
+
+    let end = intervals[0][1];
+
+    for(let i=1; i<intervals.length; i++){
+        const interval = intervals[i];
+
+        const intervalStart = interval[0];
+        const intervalEnd = interval[1];
+
+        if(intervalStart<end){
+            count++;
+            end = Math.min(intervalEnd, end);
+        }else{
+            end = intervalEnd;
+        }
+    }
+
+    return count;
+};
+
+//  time complexity = O(nlogn)
+//  space complexity = O(1)
+   
+module.exports = eraseOverlapIntervals;
+ 
---
+++ LC56_merge/index.js
@@ -0,0 +1,27 @@
+const merge = intervals => {
+   if(!intervals.length) return intervals;
+   intervals.sort((a,b)=>{
+       return a[0]-b[0];
+   })
+  
+   const res = [intervals[0]];
+   
+   for(let i = 0; i<intervals.length; i++){
+       const currentInterval = intervals[i]
+       const lastInterval = res[res.length-1];
+
+       if(currentInterval[0] <= lastInterval[1]){
+       lastInterval[1] = Math.max(currentInterval[1], lastInterval[1]);
+       }else{
+           res.push(currentInterval);
+       }
+   }
+
+   return res;
+};
+
+
+// time complexity = O(nlogn)
+// space complexity = O(1)
+
+module.exports = merge;
